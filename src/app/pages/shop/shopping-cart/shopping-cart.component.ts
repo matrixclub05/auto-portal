274a3fbe410e6569c1ea724c65ef3dc2faf20d22
Vladyslav Bezruchenko <Vladyslav.Bezruchenko@onseo.biz>
2016-12-07 15:37:36 +0200

    added getting goods

--- src/app/pages/shop/shopping-cart/shopping-cart.component.ts
+++ src/app/pages/shop/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginServiceService} from "../../../global-services/login-service.service";
+import {IGoods} from "../../../global-services/data-objects/UserData";
 
 @Component({
   selector: 'app-shopping-cart',
@@ -8,10 +9,29 @@ import {LoginServiceService} from "../../../global-services/login-service.servic
 })
 export class ShoppingCartComponent implements OnInit {
 
+  private _goods:Array<IGoods> = [];
+
+  private _cars:Array<IGoods> = [];
+  private _rest:Array<IGoods> = [];
+
   constructor(private _loginService:LoginServiceService) { }
 
   ngOnInit() {
-    this._loginService.loginData.getUserData("garageCar").shopCartData
+
+    this._goods = this._loginService.loginData.getUserData("garageCar").shopCartData;
+
+    for(var i = 0; i < this._goods.length; i++)
+    {
+      let good = this._goods[i];
+      if(good.type == "Car")
+      {
+        this._cars.push(good);
+      }
+      else
+      {
+        this._rest.push(good);
+      }
+    }
   }
 
 }
