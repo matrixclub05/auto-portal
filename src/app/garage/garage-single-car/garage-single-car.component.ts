9c01d1d3c3f0492e370211a34e49a8eb73494667
Vladyslav Bezruchenko <Vladyslav.Bezruchenko@onseo.biz>
2017-01-05 14:58:00 +0200

    implemented add car to market from garage

--- src/app/garage/garage-single-car/garage-single-car.component.ts
+++ src/app/garage/garage-single-car/garage-single-car.component.ts
@@ -1,13 +1,11 @@
 import {Component, OnInit, Input, EventEmitter, Output} from "@angular/core";
 import {CarData} from "../../global-services/data-objects/CarData";
 import {DBServiceService} from "../../global-services/dbservice.service";
-import {NgbModal, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
-import {TooMuchTimeWOServiceComponent} from "./too-much-time-woservice/too-much-time-woservice.component";
 
 @Component({
   selector: '[app-garage-single-car]',
   templateUrl: 'garage-single-car.component.html',
-  styleUrls: ['garage-single-car.component.scss']
+  styleUrls: ['garage-single-car.component.scss'],
 })
 export class GarageSingleCarComponent implements OnInit {
 
@@ -15,7 +13,7 @@ export class GarageSingleCarComponent implements OnInit {
   @Output() onSelected = new EventEmitter<CarData>();
   private _isTooMuchTimeWOServiceShown:boolean = false;
 
-  constructor(private _databaseService:DBServiceService, private _modalService:NgbModal) { }
+  constructor(private _databaseService:DBServiceService) { }
 
   ngOnInit() {
   }
@@ -27,17 +25,12 @@ export class GarageSingleCarComponent implements OnInit {
 
   public addToMarket()
   {
-    if(this._isTooMuchTimeWOServiceShown)
-    {
-      this._databaseService.addFakeCarFromGarage(this._car.manufacturer,this._car.model,this._car.year);
-    }
-    else
-    {
-      const modalRef:NgbModalRef = this._modalService.open(TooMuchTimeWOServiceComponent);
-      modalRef.componentInstance.modalRef = modalRef;
-      modalRef.componentInstance.car = this._car;
-      this._isTooMuchTimeWOServiceShown = true;
-    }
+    this._databaseService.addFakeCarFromGarage(this._car.manufacturer,this._car.model,this._car.year);
   }
 
+  protected capturePhoto($event)
+  {
+    var files = $event.target.files;
+    alert(JSON.stringify(files, null, 4));
+  }
 }
--- src/app/garage/garage.module.ts
+++ src/app/garage/garage.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
 import {GarageDataService} from "./services/garage-data.service";
 import {GarageComponent} from "./moduleView/garage.component";
 import {SignUpForServiceComponent} from "./sign-up-for-service/sign-up-for-service.component";
-import { TooMuchTimeWOServiceComponent } from './garage-single-car/too-much-time-woservice/too-much-time-woservice.component';
+import {TooMuchTimeWOServiceComponent} from "./garage-single-car/too-much-time-woservice/too-much-time-woservice.component";
 
 @NgModule({
   imports: [
