e3141311617c1a0ef2cc500598f9bd6294255c89
VNemyrovskyi <Vitalii.Nemyrovskyi@onseo.biz>
2017-01-14 12:22:38 +0200

    messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,7 +54,7 @@ import { MarketServiceBookComponent } from './components/market-car/market-servi
 import {PhotoMemoryService} from "./global-services/photo-memory.service";
 import {TestDriveComponent} from "./garage/test-drive/test-drive.component";
 import { MessagesComponent } from './pages/profile/messages/messages.component';
-import { UserInfoComponent } from './components/user-info/user-info.component';
+import {UserInfoComponent, SafeUrlPipe} from './components/user-info/user-info.component';
 import { FirmInfoComponent } from './components/firm-info/firm-info.component';
 
 
@@ -107,6 +107,7 @@ import { FirmInfoComponent } from './components/firm-info/firm-info.component';
     MarketCarComponent,
     MessagesComponent,
     UserInfoComponent,
+    SafeUrlPipe,
     FirmInfoComponent
   ],
   imports: [
