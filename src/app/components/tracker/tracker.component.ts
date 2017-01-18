53186fb60e7c966a92e56d7d5d8b60114a03fb86
Vladyslav Bezruchenko <Vladyslav.Bezruchenko@onseo.biz>
2017-01-18 23:40:06 +0200

    implemented user identification in tracker

--- src/app/components/tracker/tracker.component.ts
+++ src/app/components/tracker/tracker.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 import {UserNavigationHistoryService, TrackAction} from "../../global-services/user-navigation-history.service";
+import {LoginServiceService} from "../../global-services/login-service.service";
+import {User} from "../../global-services/data-objects/Message";
 
 @Component({
   selector: 'tracker',
   templateUrl: './tracker.component.html',
   styleUrls: ['./tracker.component.scss']
 })
-export class TrackerComponent implements OnInit {
+export class TrackerComponent implements OnInit, AfterViewInit {
 
   private _isOpen:boolean = false;
   private _actions:Array<TrackAction> = [];
+  private _currentUser:User;
 
-  constructor(private _nav:UserNavigationHistoryService)
+  constructor(private _nav:UserNavigationHistoryService, private _loginService:LoginServiceService)
   {
 
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
   }
 
   private toggle()
@@ -25,6 +31,7 @@ export class TrackerComponent implements OnInit {
     if(this._isOpen)
     {
       this._actions = this._nav.getActions();
+      this._currentUser = this._loginService.getCurrentUser();
     }
   }
 }
